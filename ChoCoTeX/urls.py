cce3c5dce6b9aed7c533a86dcab82b6b86ff136a
Damien Minenna <damien.minenna+bitbucket@gmail.com>
2021-03-20 10:22:26 +0100

    Version 1.0.1

--- ChoCoTeX/urls.py
+++ ChoCoTeX/urls.py
@@ -15,6 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, re_path
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from ChoCoTeX_app import views
 
--- ChoCoTeX_app/views.py
+++ ChoCoTeX_app/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 import os
 from os import listdir
